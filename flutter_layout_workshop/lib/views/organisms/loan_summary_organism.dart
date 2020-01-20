e61c2325328b07b744b2bf8eb83bf036bd5ca284
Krittayot (Benz) Techasombooranakit <ktechasombooranakit@palo-it.com>
2020-01-20 15:21:51 +0700

    changes

--- flutter_layout_workshop/lib/views/organisms/loan_summary_organism.dart
+++ flutter_layout_workshop/lib/views/organisms/loan_summary_organism.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_workshop/views/modules/labels/compare_loan_label.dart';
 import 'package:flutter_layout_workshop/views/modules/loan_summary_card.dart';
-import 'package:flutter_layout_workshop/views/modules/loan_summary_content.dart';
-
 import 'loan_summart_content_organism.dart';
 
 class LoanSummaryOrganism extends StatelessWidget {

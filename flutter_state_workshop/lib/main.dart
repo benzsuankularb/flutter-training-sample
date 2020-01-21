05571c473ca6d57918c52cf8836a452d206964ec
Krittayot (Benz) Techasombooranakit <ktechasombooranakit@palo-it.com>
2020-01-21 16:39:14 +0700

    workshops

--- flutter_state_workshop/lib/main.dart
+++ flutter_state_workshop/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:mobile/app.dart'
     show setArgs, addCrossDependency, startApp, startSystem;
 import 'package:mobile/stores/app_store.dart';
+import 'package:mobile/stores/loan_store.dart';
 import 'package:mobile/systems/main_system.dart';
 import 'package:mobile/views/main.dart';
 
@@ -14,4 +15,5 @@ main() {
 void addStores() {
   // Add stores belows
   addCrossDependency(AppStore());
+  addCrossDependency(LoanStore());
 }
